a7351882373694dd8460f92045d3ee0c65c62103
Whiskeryfrog <129975894+Whiskeryfrog@users.noreply.github.com>
2024-04-04 22:43:00 +0800

    Add files via upload

---
+++ playthroughs/pokemonleafgreennuzlocke/changetab.js
@@ -0,0 +1,57 @@
+var currentSelection = 'Episodes'; // Initially set the button text to 'Episodes'
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function changeTab(selectedValue) {
+    var tabContent = document.getElementsByClassName("tabcontent");
+
+    for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    document.getElementById(selectedValue).style.display = "block";
+    document.getElementById("myDropdown").classList.remove("show"); // Hide dropdown after selection
+
+    // Update dropdown button text with selected option
+    currentSelection = selectedValue;
+    document.getElementById("dropdownButton").textContent = currentSelection;
+}
+
+function toggleRules() {
+    const rulesDiv = document.getElementById("displayrules");
+
+    if (rulesDiv.offsetHeight === 0) {
+        rulesDiv.style.height = rulesDiv.scrollHeight + "px";
+    } else {
+        rulesDiv.style.height = 0;
+    }
+}
+
+function toggleextraRules() {
+    const rulesDiv = document.getElementById("displayextrarules");
+
+    if (rulesDiv.offsetHeight === 0) {
+        rulesDiv.style.height = rulesDiv.scrollHeight + "px";
+    } else {
+        rulesDiv.style.height = 0;
+    }
+}
+
